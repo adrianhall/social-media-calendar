83b57448f14bc2d01f7488a540f1758e07625161
Adrian Hall <adhal@microsoft.com>
2024-06-26 15:56:33 -0700

    Added react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import SocialMediaCalendar from './social-media-calendar/Main.tsx';
-import { store } from './redux/store';
+import SocialMediaCalendar from './app/App';
+import { store } from './app/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <SocialMediaCalendar />
     </Provider>
-    <App />
   </React.StrictMode>,
 )
